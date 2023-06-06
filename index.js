9cc5af7b8f34437cc9b30ca5fd2215cb0fd7cdf3
Saurabh Tripathi <saurabh.umesh.tripathi@gmail.com>
2023-06-06 12:05:57 +0530

    index.js update 1.1

--- index.js
+++ index.js
@@ -7,48 +7,63 @@ import data from "./data.js"
 
 const acceptBtn = document.getElementById('accept')
 const rejectBtn = document.getElementById('reject')
-
+const dogObj = new Dog(data[0])
 
 function render() {
-    let dogObj = new Dog(data[0])
-    document.getElementById('dog-desc').innerHTML = dogObj.getDogHtml()
-    document.onclick = e => {
-    let btnClicked = e.target.id
-    if(btnClicked === 'accept'){
-        hasBeenLiked = true
-        hasBeenSwiped = true
-        document.getElementById('dog-desc').innerHTML = ""
-        document.getElementById('dog-desc').innerHTML = dogObj.getDogHtml()
-        document.getElementById('dog-desc').innerHTML  += dogObj.getLikeBadgeHtml()                                                   
-    }else if (btnClicked === 'reject' && hasBeenLiked){
-        hasBeenLiked = false
-        hasBeenSwiped = true
-        document.getElementById('dog-desc').innerHTML = ""
-        document.getElementById('dog-desc').innerHTML = dogObj.getDogHtml()
-        document.getElementById('dog-desc').innerHTML  += dogObj.getNopeBadgeHtml()                                                  
-    }else if (btnClicked === 'reject' && !hasBeenLiked) {
-        hasBeenSwiped = true
-        document.getElementById('dog-desc').innerHTML  += dogObj.getNopeBadgeHtml()
-    }
-
-} 
-    swipeHorizontally()
+    document.getElementById('dog-desc').innerHTML = getNewDog().getDogHtml()
+    swipeVertically()
 }
-function swipeHorizontally() {
-    let touchstartX = 0
-    let touchendX = 0
+
+function getNewDog(){
+    const nextDog = data.shift()
+    return nextDog ? new Dog(nextDog) : {}
+}
+
+
+
+
+    //     document.onclick = e => {
+    //     let btnClicked = e.target.id
+    //     if(btnClicked === 'accept'){
+    //         hasBeenLiked = true
+    //         hasBeenSwiped = true
+    //         document.getElementById('dog-desc').innerHTML = ""
+    //         document.getElementById('dog-desc').innerHTML = dogObj.getDogHtml()
+    //         document.getElementById('dog-desc').innerHTML  += dogObj.getLikeBadgeHtml()                                                   
+    //     }else if (btnClicked === 'reject' && hasBeenLiked){
+    //         hasBeenLiked = false
+    //         hasBeenSwiped = true
+    //         document.getElementById('dog-desc').innerHTML = ""
+    //         document.getElementById('dog-desc').innerHTML = dogObj.getDogHtml()
+    //         document.getElementById('dog-desc').innerHTML  += dogObj.getNopeBadgeHtml()                                                  
+    //     }else if (btnClicked === 'reject' && !hasBeenLiked) {
+    //         hasBeenSwiped = true
+    //         document.getElementById('dog-desc').innerHTML  += dogObj.getNopeBadgeHtml()
+    //     }
+    
+    // } 
+
+
+
+
+
+
+
+function swipeVertically() {
+    let touchstartY = 0
+    let touchendY = 0
 
     function checkDirection() {
-        if (touchendX < touchstartX) alert('swiped left!')
-        if (touchendX > touchstartX) alert('swiped right!')
+        if (touchendY < touchstartY) alert('swiped DOWN!')
+        if (touchendY > touchstartY) alert('swiped UP!')
     }
 
     document.addEventListener('touchstart', e => {
-        touchstartX = e.changedTouches[0].screenX
+        touchstartY = e.changedTouches[0].screenY
     })
 
     document.addEventListener('touchend', e => {
-        touchendX = e.changedTouches[0].screenX
+        touchendY = e.changedTouches[0].screenY
         checkDirection()
     })
 }
